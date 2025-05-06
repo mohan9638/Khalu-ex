bb4c73739ae39e60baf46769cbf3930437afc762
mohan9638 <158275364+mohan9638@users.noreply.github.com>
2025-05-06 15:51:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+API_ID = os.environ.get("API_ID", "25693368")
+
+API_HASH = os.environ.get("API_HASH", "2dcf91b0f99c0b9d4875e87020e6bd07")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7715035021:AAF5l_LlyVDVIPGz18gb1Y5jZVXIVcC0kwA")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+#OWNER = int(os.environ.get("OWNER", 5917152380,6302749609))
+
+#LOG = -1002611586324
+
+# UPDATE_GRP = , # bot sat group
+
+# auth_chats = []
+
+#try:
+    #ADMINS=[5917152380,6302749609]
+    #for x in (os.environ.get("ADMINS", "5917152380,6302749609").split()):
+        #ADMINS.append(int(x))
+#except ValueError:
+        #raise Exception("Your Admins list does not contain valid integers.")
+#ADMINS.append(OWNER)
